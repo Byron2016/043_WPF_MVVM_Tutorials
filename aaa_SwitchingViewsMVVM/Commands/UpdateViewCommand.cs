3814beb2663ff88b8cc646cc4dcd0227ad2bd39a
Byron 2016 <bgva2005@yahoo.com>
2022-03-14 16:49:39 -0500

    feat: 001 create command class

---
+++ aaa_SwitchingViewsMVVM/Commands/UpdateViewCommand.cs
@@ -0,0 +1,32 @@
+﻿using aaa_SwitchingViewsMVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace aaa_SwitchingViewsMVVM.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private readonly MainViewModel _viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
