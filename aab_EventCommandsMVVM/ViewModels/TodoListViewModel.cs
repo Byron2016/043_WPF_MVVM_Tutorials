4214cd82a57ff4d58ef6e583c9acb1ea5dfc7b68
Byron 2016 <bgva2005@yahoo.com>
2022-03-14 20:24:22 -0500

    feat: 002 initial status

---
+++ aab_EventCommandsMVVM/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using aab_EventCommandsMVVM.Commands;
+using aab_EventCommandsMVVM.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace aab_EventCommandsMVVM.ViewModels
+{
+    public class TodoListViewModel : ObservableObject
+	{
+		private ObservableCollection<TodoItem> _todoItems;
+		public ObservableCollection<TodoItem> TodoItems
+		{
+			get
+			{
+				return _todoItems;
+			}
+			set
+			{
+				_todoItems = value;
+				OnPropertyChanged(nameof(TodoItems));
+			}
+		}
+
+		public ICommand LoadTodoItemsCommand { get; set; }
+
+		public TodoListViewModel()
+		{
+			LoadTodoItemsCommand = new LoadTodoItemsCommand(this);
+		}
+	}
+}
--- aab_EventCommandsMVVM/Views/TodoList.xaml.cs
+++ aab_EventCommandsMVVM/Views/TodoList.xaml.cs
@@ -1,5 +1,4 @@
-﻿using aab_EventCommandsMVVM.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,18 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace aab_EventCommandsMVVM
+namespace aab_EventCommandsMVVM.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TodoList.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TodoList : UserControl
     {
-        public MainWindow()
+        public TodoList()
         {
             InitializeComponent();
-
-            DataContext = new MainViewModel();
         }
     }
 }
